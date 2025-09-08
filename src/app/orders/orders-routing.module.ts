16daddb6db6f766351a9b3d964cf2477a28dd307
leandro <lgomesb@gmail.com>
2025-09-08 10:08:36 +0100

    Create routing for orders Form

--- src/app/orders/orders-routing.module.ts
+++ src/app/orders/orders-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrdersListComponent } from './orders-list/orders-list.component';
+import { OrdersFormComponent } from './orders-form/orders-form.component';
 
 const routes: Routes = [
   { path: 'orders-list', component: OrdersListComponent },
+  { path: 'orders-form', component: OrdersFormComponent }
 ];
 
 @NgModule({
