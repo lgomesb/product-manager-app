7ba792b1bc4821698d6843f651c0e3c92180029d
leandro <lgomesb@gmail.com>
2025-09-28 22:52:03 +0100

    Disable search product given category is null

--- src/app/orders/orders-form/add-product-order-item-dialog/add-product-order-item-dialog.component.ts
+++ src/app/orders/orders-form/add-product-order-item-dialog/add-product-order-item-dialog.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Category } from 'src/app/categories/category';
 import { Product } from 'src/app/products/product';
+import { FindProductsDialogComponent } from 'src/app/products/products-dialog/find-products-dialog/find-products-dialog.component';
 import { FindCategoriesDialogComponent } from 'src/app/products/products-form/find-categories-dialog/find-categories-dialog.component';
+import { ProductOrderItem } from '../../productOrderItem';
 
 @Component({
   selector: 'app-add-product-order-item-dialog',
@@ -12,20 +14,24 @@ import { FindCategoriesDialogComponent } from 'src/app/products/products-form/fi
 export class AddProductOrderItemDialogComponent {
 
   product: Product = new Product;
+  quantity!: number; 
   category!: Category;
 
-  constructor(private dialog: MatDialog) {
+  constructor(
+    private dialog: MatDialog, 
+    public dialogRef: MatDialogRef<AddProductOrderItemDialogComponent>
+  ) {
 
   }
 
 
-  openSearchDialog() {
-    const dialogRef = this.dialog.open(FindCategoriesDialogComponent, {
+  openCategoriesSearchDialog() {
+    const categoryDialog = this.dialog.open(FindCategoriesDialogComponent, {
       width: '600px',
       data: {} // dados opcionais
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    categoryDialog.afterClosed().subscribe(result => {
       if (result) {
         this.category = new Category();
         this.category.id = result.id;
@@ -36,6 +42,51 @@ export class AddProductOrderItemDialogComponent {
       }
     });
 
+  }
+
+  openProductsSearchDialog() {
+    const productDialog = this.dialog.open(FindProductsDialogComponent, {
+      width: '600px',
+      data: this.category.id
+    });
+
+    productDialog.afterClosed().subscribe(result => {
+      if (result) {
+        this.product = new Product();
+        this.product.id = result.id;
+        this.product.name = result.name;
+        this.product.category = this.category;
+
+        console.log(`Produto selecionado: ${result.name}`);
+      } else {
+        console.log('Nenhum product foi selecionada.');
+      }
+    });
+
+  }
+
+  cancel() {
+    this.dialog.closeAll();
+  }
+
+  onAddClicked() {
+    let productOrderItem = ProductOrderItem.create(this.product, this.quantity);
+
+    this.dialogRef.close(productOrderItem);
+  }
+
+  blockNegative(event: KeyboardEvent) {
+    if (event.key === '-' || event.key === 'e' || event.key === '+' || event.key === '.') {
+      event.preventDefault();
+    }
+  }
+
+  onInputChange() {
+    if (isNaN(this.quantity)) {
+      this.quantity = 1;
+    } else if (this.quantity < 1) {
+      this.quantity = 1;
+    }
   }
 
 }
